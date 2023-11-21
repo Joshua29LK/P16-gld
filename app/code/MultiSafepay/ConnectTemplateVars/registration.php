cfbd22f048fe045f35cadc3de044202084936406
prod <info@medeka.nl>
2023-11-21 14:49:39 +0100

    install MSP 3.2.1

--- app/code/MultiSafepay/ConnectTemplateVars/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright © MultiSafepay All rights reserved.
- * See COPYING.txt for license details.
- */
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'MultiSafepay_ConnectTemplateVars', __DIR__);
-
