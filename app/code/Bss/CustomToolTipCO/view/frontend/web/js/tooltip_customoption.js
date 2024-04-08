6171179119db5471232bb0865adc0892bb144a81
prod <info@medeka.nl>
2024-04-08 12:41:32 +0200

    fix tooltip radio buttons

--- app/code/Bss/CustomToolTipCO/view/frontend/web/js/tooltip_customoption.js
+++ app/code/Bss/CustomToolTipCO/view/frontend/web/js/tooltip_customoption.js
@@ -30,12 +30,22 @@ define([
         _RenderToolTip: function () {
             var jsonTooltip = this.options.jsonTooltip;
             var $widget = this;
+            var tooltipRadio = 0;
             $('.product-custom-option').each(function () {
                 var optionid = $(this).attr('name').match(/\d+/)[0];
                 $modalOption = optionid
                 var tooltip_content = jsonTooltip[optionid];
                 if (tooltip_content && tooltip_content.trim() != '') {
-                    $(this).closest(".field").find('label').after(`<span class="bss-tooltip-content"><span class="bss-tooltip-customoption" id="`+optionid+`"><i class="far fa-question-circle"></i></span></span>`);
+                    var bssOption = $(this).closest(".field");
+                    if (this.type == 'radio') {
+                        bssOption = $(this).closest(`#bss_options_${optionid}`);
+                        if (tooltipRadio !== optionid) {
+                            bssOption.children('label').after(`<span class="bss-tooltip-content"><span class="bss-tooltip-customoption" id="`+optionid+`"><i class="far fa-question-circle"></i></span></span>`);
+                            tooltipRadio = optionid;
+                        }
+                    } else {
+                        bssOption.children('label').after(`<span class="bss-tooltip-content"><span class="bss-tooltip-customoption" id="`+optionid+`"><i class="far fa-question-circle"></i></span></span>`);
+                    }
                 }
                 $(this).append(`<div id="modal-content-`+optionid+`">
                     <div class="modal-inner-content">
